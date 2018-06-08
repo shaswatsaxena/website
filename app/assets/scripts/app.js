eb4d489b5188359f6418d6fa49354f20ab29feb0
shaswatsaxena <shaswatsaxena@gmail.com>
2018-06-08 12:27:55 +0530

    Completed Reveal on Scroll Behaviour

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var featureOnScroll = new RevealOnScroll(".feature-item","85%");
+var testimonialOnScroll = new RevealOnScroll(".testimonial","60%");
