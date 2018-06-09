24fada81a3dd8c64d58e3d41d1a4f78b866a7aeb
shaswatsaxena <shaswatsaxena@gmail.com>
2018-06-09 16:22:10 +0530

    Completed Modal Overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var featureOnScroll = new RevealOnScroll(".feature-item","85%");
 var testimonialOnScroll = new RevealOnScroll(".testimonial","60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
