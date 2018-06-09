9b5c263bb2fa7d4bef70d73d66f3b34ce1a749a7
shaswatsaxena <shaswatsaxena@gmail.com>
2018-06-09 13:31:32 +0530

    Completed Sticky Header"

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var featureOnScroll = new RevealOnScroll(".feature-item","85%");
 var testimonialOnScroll = new RevealOnScroll(".testimonial","60%");
+var stickyHeader = new StickyHeader();
